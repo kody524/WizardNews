1ba78198dd077e7450c44aff4630d0c51612e01b
kody524 <kody524@icloud.com>
2023-01-05 19:05:12 -0600

    almost done

--- app.js
+++ app.js
@@ -1,7 +1,80 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan")
+app.use(express.static('public'))
+const postBank = require("./postBank")
+app.use(morgan('dev'));
+app.get("/", (req, res) =>{
+const posts = postBank.list()
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const html = `<!DOCTYPE html>
+<html>
+<head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+  <div class="news-list">
+    <header><img src="/logo.png"/>Wizard News</header>
+    ${posts.map(post => `
+      <div class='news-item'>
+        <p>
+          <span class="news-position">${post.id}. ▲</span>
+          <a href="/posts/${post.id}">${post.title}</a>
+          <small>(by ${post.name})</small>
+        </p>
+        <small class="news-info">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+      </div>`
+    ).join('')}
+  </div>
+</body>
+</html>`
+res.send(html);
+})
+
+app.get('/posts/:id', (req, res) => {
+
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+   
+    throw new Error(
+      app.use((err, req, res, next) => {
+        console.error(err.stack)
+        res.status(500).send('Something broke!')
+      })
+    )
+  }
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <link rel="stylesheet" href="/style.css" />
+  <title>Wizard News></title>
+  </head>
+  <body>
+  <div class="news-list">
+  <header><img src="/logo.png"/>Wizard News</header>
+  <div class='news-item'>
+  <p>
+    <span class="news-position">${post.id}. ▲</span>
+    <span>${post.title}</span>
+    <small>(by ${post.name})</small>
+  </p>
+  <br></br>
+  <div>${post.content}</div>
+  <small class="news-info">
+  ${post.upvotes} upvotes | ${post.date}
+</small>
+</div>
+  </body>
+  </html>
+
+  `
+  res.send(html);
+});
+ 
 
 const PORT = 1337;
 
